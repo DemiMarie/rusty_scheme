ebb3ddfea3700ef854b956a004c6b404fbe44854
Demi Obenour <demiobenour@gmail.com>
2016-06-25 16:34:43 -0400

    Fixed memcpy_gc and added alloc_vector
    This commit fixes memcpy_gc (now called memcpy-gc) and adds a function
    to allocate a pair on the GC heap.

--- src/alloc.rs
+++ src/alloc.rs
@@ -38,6 +38,7 @@ pub trait Allocator {
 // /// Allocates a boxed float on the top of the stack.
 // fn alloc_float(&mut self, float: f64) -> value::Float;
 }
+
 #[derive(Debug)]
 pub struct Heap {
     tospace: Vec<Value>,
@@ -50,9 +51,11 @@ pub struct Heap {
 unsafe fn consistency_check(heap: &Vec<Value>) {
     let assert_in_heap = |heap: &Vec<_>, ptr: usize| {
         assert!(ptr >= heap.as_ptr() as usize);
-        let upper_limit = heap.as_ptr() as usize + heap.capacity()*size_of!(Value);
+        let upper_limit = heap.as_ptr() as usize + heap.capacity() * size_of!(Value);
         assert!(ptr < upper_limit,
-                "Heap pointer out of range: {} >= {}", ptr, upper_limit)
+                "Heap pointer out of range: {} >= {}",
+                ptr,
+                upper_limit)
     };
     let mut index = 0;
     while index < heap.len() {
@@ -70,8 +73,7 @@ unsafe fn consistency_check(heap: &Vec<Value>) {
                     assert!(current.contents & 0b111 == 0b111);
                     assert!((*Ptr_Val!(current)).contents == PAIR_HEADER);
                     for i in 1..3 {
-                        assert_in_heap(heap, Ptr_Val!(current).offset(i)
-                                       as usize)
+                        assert_in_heap(heap, Ptr_Val!(current).offset(i) as usize)
                     }
                 }
                 Tags::Vector => {
@@ -86,9 +88,9 @@ unsafe fn consistency_check(heap: &Vec<Value>) {
         }
     }
 }
+
 #[cfg(not(debug_assertions))]
 unsafe fn consistency_check(_heap: &Vec<Value>) {}
-
 
 /// Relocates a `Value` in the heap.
 ///
@@ -97,13 +99,15 @@ unsafe fn consistency_check(_heap: &Vec<Value>) {}
 /// end of tospace.
 ///
 /// This function takes raw pointers because of aliasing concerns.
-unsafe fn relocate(current: *mut Value,
-                   tospace: &mut Vec<Value>,
-                   fromspace: &mut Vec<Value>) {
+unsafe fn relocate(current: *mut Value, tospace: &mut Vec<Value>, fromspace: &mut Vec<Value>) {
+    debug_assert!(tospace.capacity() >= fromspace.len());
+    debug!("Tospace capacity: {}, Fromspace length: {}",
+           tospace.capacity(),
+           fromspace.len());
     let size_of_value: usize = size_of!(Value);
     (*current).size().map(|size| {
         // pointer to head of object being copied
-        let pointer = Ptr_Val!(*current);
+        let pointer: *mut Value = Ptr_Val!(*current);
 
         // Assert that the object header is nonzero.
         debug_assert!((*pointer).contents != 0,
@@ -113,47 +117,42 @@ unsafe fn relocate(current: *mut Value,
             // since no object can have a size of zero).
             *current = *pointer.offset(1)
         } else {
+            let len = tospace.len();
+
             // End pointer
-            let end = tospace.as_mut_ptr().offset(tospace.len() as isize);
+            let end = tospace.as_mut_ptr().offset(len as isize);
 
-            if cfg!(feature = "memcpy_gc") {
-                debug_assert!(end as usize & 0b111 == 0);
+            let amount_to_copy = ((size * size_of_value + 0b111) & !0b111) / size_of_value;
+
+            // Check that the amount to copy is reasonable
+            debug_assert!(amount_to_copy > 0);
+
+            // Check that the end pointer is aligned
+            debug_assert!(end as usize & 0b111 == 0);
+
+            // Check that the pointer really is to fromspace
+            debug_assert!((pointer as usize) <
+                          (fromspace.as_ptr() as usize + fromspace.len() * size_of!(usize)));
+            debug_assert!(pointer as usize >= fromspace.as_ptr() as usize);
+
+            if cfg!(feature = "memcpy-gc") {
+                let words_to_copy = amount_to_copy * size_of_value;
                 // The amount to copy
-                let amount_to_copy = (size * size_of_value + 0b111) & !0b111;
-                debug_assert!(end as usize + amount_to_copy <=
-                              tospace.as_ptr() as usize + tospace.capacity());
-                debug_assert!(pointer as usize >= end as usize + amount_to_copy ||
-                              pointer as usize + amount_to_copy <= end as usize);
+                debug_assert!(amount_to_copy + len <= tospace.capacity());
+                debug_assert!(pointer as usize >= end as usize + words_to_copy ||
+                              pointer as usize + words_to_copy <= end as usize);
+                // NOTE: reverse pointer argument order from `memcpy`.
                 ptr::copy_nonoverlapping(pointer, end, amount_to_copy);
-                *current = Value { contents: end as usize | (*current).raw_tag() };
-                let len = tospace.len();
-                tospace.set_len(len + amount_to_copy / size_of_value);
+                tospace.set_len(len + amount_to_copy)
             } else {
-                let amount_to_copy = ((size * size_of_value + 0b111) & !0b111) / size_of_value;
-
-                // Check that the amount to copy is reasonable
-                debug_assert!(amount_to_copy > 0);
-
-                // Check that the end pointer is aligned
-                debug_assert!(end as usize & 0b111 == 0);
-
-                let ptr = Ptr_Val!(*current);
-
-                // Check that the pointer really is to tospace
-                debug_assert!((ptr as usize) <
-                              (fromspace.as_ptr() as usize +
-                               fromspace.len()*size_of!(usize)));
-                debug_assert!(ptr as usize >= fromspace.as_ptr() as usize);
                 // NOTE: this MUST come before replacing the old object with
                 // a forwarding pointer – otherwise, this replacement will
                 // clobber the copied object's header!
                 tospace.extend(slice::from_raw_parts(pointer, amount_to_copy));
-                *ptr = Value{ contents: HEADER_TAG, };
-                *current = Value { contents: end as usize |
-                                        ((*current).contents & 0b111) };
-                *ptr.offset(1) = Value{ contents: end as usize };
-                *ptr.offset(1) = *current;
             }
+            *pointer = Value { contents: HEADER_TAG };
+            *current = Value { contents: end as usize | ((*current).contents & 0b111) };
+            *pointer.offset(1) = *current;
         }
     });
 }
@@ -175,6 +174,7 @@ unsafe fn scavange_heap(tospace: &mut Vec<Value>, fromspace: &mut Vec<Value>) {
     }
 }
 
+/// Handles all of the data on the stack.
 unsafe fn scavange_stack(stack: &mut Vec<Value>,
                          tospace: &mut Vec<Value>,
                          fromspace: &mut Vec<Value>) {
@@ -183,41 +183,38 @@ unsafe fn scavange_stack(stack: &mut Vec<Value>,
     }
 }
 
+/// Performs a full garbage collection
 fn collect(heap: &mut Heap) {
-    println!("Initiated garbage collection");
+    debug!("Initiated garbage collection");
     unsafe {
         consistency_check(&heap.tospace);
-        println!("Completed first consistency check");
+        debug!("Completed first consistency check");
         mem::swap(&mut heap.tospace, &mut heap.fromspace);
-        heap.tospace.reserve(heap.fromspace.len() + heap.fromspace.len()/2);
-        println!("Fromspace size is {}", heap.fromspace.len() + heap.fromspace.len() / 2);
-        heap.tospace.resize(0, Value{contents:0});
-        println!("Tospace resized to {}", heap.tospace.capacity());
+        heap.tospace.reserve(heap.fromspace.len() + heap.fromspace.len() / 2);
+        debug!("Fromspace size is {}",
+               heap.fromspace.len() + heap.fromspace.len() / 2);
+        heap.tospace.resize(0, Value { contents: 0 });
+        debug!("Tospace resized to {}", heap.tospace.capacity());
         let _ = stdout().flush();
         scavange_stack(&mut heap.stack, &mut heap.tospace, &mut heap.fromspace);
-        println!("Stack scavanged");
+        debug!("Stack scavanged");
         scavange_heap(&mut heap.tospace, &mut heap.fromspace);
-        println!("Heap scavanged");
+        debug!("Heap scavanged");
         consistency_check(&heap.tospace);
-        println!("Completed second consistency check");
-        heap.fromspace.resize(0, Value{contents: 0});
+        debug!("Completed second consistency check");
+        heap.fromspace.resize(0, Value { contents: 0 });
     }
 }
 
+/// Represents the stack.
 #[derive(Debug)]
 pub struct Stack {
     pub innards: Vec<value::Value>,
 }
-#[cfg(none)]
-impl ::std::ops::Index<usize> for Stack {
-    type Output = value::Value;
-    fn index(&self, other: u8) -> &value::Value {
-        let val: usize = other.into();
-        &self.innards[val]
-    }
-}
 
 use std::ops::{Deref, DerefMut};
+
+/// A `Stack` acts like a `Vec`.
 impl Deref for Stack {
     type Target = Vec<value::Value>;
     fn deref(&self) -> &Vec<value::Value> {
@@ -225,6 +222,7 @@ impl Deref for Stack {
     }
 }
 
+/// A `Stack` acts like a `Vec`.
 impl DerefMut for Stack {
     fn deref_mut<'a>(&'a mut self) -> &'a mut Vec<value::Value> {
         &mut self.innards
@@ -248,8 +246,23 @@ impl Heap {
             },
         };
         self.stack.push(new_value);
-        println!("Allocated a pair");
+        debug!("Allocated a pair")
     }
+
+    pub fn alloc_vector(&mut self, elements: &[Value]) {
+        let len = elements.len();
+        let tospace_space = self.tospace.capacity() - self.tospace.len();
+        if tospace_space < (elements.len() + 0b111) & !0b111 {
+            collect(self)
+        }
+        self.tospace.push(Value { contents: value::VECTOR_HEADER | elements.len() });
+        let ptr = unsafe {
+            self.tospace.as_ptr().offset(len as isize) as usize | value::VECTOR_TAG
+        };
+        self.tospace.extend(elements);
+        self.stack.push(Value { contents: ptr });
+    }
+
     pub fn new(size: usize) -> Self {
         Heap {
             fromspace: Vec::with_capacity(size),
@@ -268,12 +281,12 @@ mod tests {
     fn can_allocate_objects() {
         let mut heap = Heap::new(1 << 4);
         super::collect(&mut heap);
-        println!("HEADER_TAG = {:x}, PAIR_TAG = {:x}, SIZEOF_PAIR = {:x}",
-                 HEADER_TAG,
-                 PAIR_HEADER,
-                 SIZEOF_PAIR);
+        debug!("HEADER_TAG = {:x}, PAIR_TAG = {:x}, SIZEOF_PAIR = {:x}",
+               HEADER_TAG,
+               PAIR_HEADER,
+               SIZEOF_PAIR);
         heap.alloc_pair(ZERO, ZERO);
-        //println!("{:?}", heap);
+        // debug!("{:?}", heap);
         for i in 1..((1 << 11)) {
             let old_pair = heap.stack[0];
             heap.alloc_pair(old_pair, old_pair);
@@ -286,7 +299,7 @@ mod tests {
                 assert_eq!(new_pair.size(), Some(3));
                 if let EnumValue::Pair(ptr) = new_pair.enum_type() {
                     assert_eq!((unsafe { (*ptr).car.get().tag() }), Tags::Pair);
-                    assert_eq!((unsafe { (*ptr).cdr.get().tag() }), Tags::Pair);
+                    assert_eq!((unsafe { (*ptr).cdr.get().tag() }), Tags::Pair)
                 } else {
                     unreachable!()
                 }
@@ -294,15 +307,13 @@ mod tests {
             assert_valid(&heap);
             super::collect(&mut heap);
             assert_valid(&heap);
-            assert!(heap.tospace.len() >= 3*i)
+            assert!(heap.tospace.len() >= 3 * i)
         }
         heap.stack.pop();
         assert!(heap.stack.len() == 0);
         // assert!(heap.fromspace.capacity() > 3* (1 << 20));
-        //println!("{:?}", heap);
-        {
-            super::collect(&mut heap);
-            assert!(heap.tospace.len() == 0);
-        }
+        // debug!("{:?}", heap);
+        super::collect(&mut heap);
+        assert!(heap.tospace.len() == 0)
     }
 }
